8dd4afacc49bb97d0f190776d736f590b884d2b5
Saad Hassan <saad.hassan@launchbox.pk>
2024-03-05 19:57:16 +0500

    Section 10 completed

--- app.js
+++ app.js
@@ -4,15 +4,37 @@ const userRouter = require('./routes/user_routes');
 const morgan = require('morgan');
 const AppError = require('./utils/app_error');
 const errorFunction = require('./controllers/error_controller');
+const rateLimit = require('express-rate-limit');
+const helmet = require('helmet');
+const mongoSanitize = require('express-mongo-sanitize');
+// const xss = require('xss-clean');
 
 const app = express();
+
+//HTTP security headers
+app.use(helmet());
 
 //3rd party middleware
 if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
 }
-//default middle ware
-app.use(express.json());
+
+//Rate Limiting
+const limiter = rateLimit({
+  max: 100,
+  windowMs: 60 * 60 * 1000,
+  message: 'Too many request from this IP, please try again later',
+});
+
+app.use('/api', limiter);
+
+//default middle ware/Body parser and date limiter
+app.use(express.json({ limit: '10Kb' }));
+//data sanitization
+app.use(mongoSanitize());
+// prevention against cross scripting attack
+// app.use(xss());
+
 app.use(express.static(`${__dirname}/public`));
 
 app.use('/api/v1/tours', tourRouter);
